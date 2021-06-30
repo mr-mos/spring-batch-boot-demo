c8a4d0a79e1a6ccc43a5bd21cbd8c5e365a876b5
Maxi Kraft <kraft.maxi@gmail.com>
2021-06-30 16:57:07 +0200

    First try database connection

--- src/main/java/de/moscon/etl/config/SpringConfig.java
+++ src/main/java/de/moscon/etl/config/SpringConfig.java
@@ -1,9 +1,13 @@
 package de.moscon.etl.config;
 
+import de.moscon.etl.beans.Customer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
@@ -16,17 +20,36 @@ public class SpringConfig {
 	@Value("org/springframework/batch/core/schema-drop-sqlite.sql")
 	private Resource dropReopsitoryTables;
 
+
 	@Value("org/springframework/batch/core/schema-sqlite.sql")
 	private Resource dataReopsitorySchema;
 
 
+//    private static NamedParameterJdbcTemplate jdbcTemplate;
+//
+//	@Bean
+//	NamedParameterJdbcTemplate jdbcTemplate(DataSource dataSource) {
+//		return new NamedParameterJdbcTemplate(dataSource);
+//	}
+
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.sqlite.JDBC");
-		dataSource.setUrl("jdbc:sqlite:repository.sqlite");
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/lab806");
+		dataSource.setUsername("root");
+		dataSource.setPassword("#t2rfortheMax");
 		return dataSource;
 	}
+
+//    @Bean
+//	ItemWriter<Customer> csvFileDatabaseItemWriter(DataSource dataSource, NamedParameterJdbcTemplate jdbcTemplate) {
+//        JdbcBatchItemWriter<Customer> databaseItemWriter = new JdbcBatchItemWriter<>();
+//        databaseItemWriter.setDataSource(dataSource);
+//        databaseItemWriter.setJdbcTemplate(jdbcTemplate);
+//        return databaseItemWriter;
+//    }
+
 
 
 	@Bean
@@ -34,16 +57,20 @@ public class SpringConfig {
 		ResourceDatabasePopulator databasePopulator =
 				new ResourceDatabasePopulator();
 
-		databasePopulator.addScript(dropReopsitoryTables);
-		databasePopulator.addScript(dataReopsitorySchema);
-		databasePopulator.setIgnoreFailedDrops(true);
+			databasePopulator.addScript(dropReopsitoryTables);
+			databasePopulator.addScript(dataReopsitorySchema);
+			databasePopulator.setIgnoreFailedDrops(true);
 
-		DataSourceInitializer initializer = new DataSourceInitializer();
-		initializer.setDataSource(dataSource);
-		initializer.setDatabasePopulator(databasePopulator);
 
-		return initializer;
+			DataSourceInitializer initializer = new DataSourceInitializer();
+			initializer.setDataSource(dataSource);
+			initializer.setDatabasePopulator(databasePopulator);
+
+			return initializer;
+		}
+
 	}
 
 
-}
+
+
--- src/main/java/de/moscon/etl/steps/nr01_customerData/CustomerWriter.java
+++ src/main/java/de/moscon/etl/steps/nr01_customerData/CustomerWriter.java
@@ -1,28 +1,51 @@
 package de.moscon.etl.steps.nr01_customerData;
 
 import de.moscon.etl.beans.Customer;
-import de.moscon.etl.steps.StepUtils;
-import org.springframework.batch.item.file.FlatFileItemWriter;
+
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.stereotype.Component;
+import org.springframework.core.io.Resource;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
 @Component
-public class CustomerWriter extends FlatFileItemWriter<Customer> {
+public class CustomerWriter extends JdbcBatchItemWriter<Customer> {
 
 	private Resource outputResource = new FileSystemResource("data/output/kunden_tennisshop.csv");
 
-	public CustomerWriter() {
-		setResource(outputResource);
-		// setAppendAllowed(true);
-		setLineAggregator(StepUtils.createLineAggregator(getFields()));
-		setHeaderCallback(writer -> {
-			writer.write(String.join(";",getFields()));
+// public CustomerWriter() {
+//    setResource(outputResource);
+//    // setAppendAllowed(true);
+//    setLineAggregator(StepUtils.createLineAggregator(getFields()));
+//    setHeaderCallback(writer -> {
+//       writer.write(String.join(";",getFields()));
+//    });
+// }
+//
+//
+// private String[] getFields() {
+//    return new String[]{"id", "pseudonym", "gender", "birthdayFormatted", "zipCode", "city", "registrationDateFormatted"};
+// }
+
+	@Override
+	public void write(List<? extends Customer> items) throws Exception {
+		namedParameterJdbcTemplate.getJdbcOperations().batchUpdate("lab806", new BatchPreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				// set values on your sql
+			}
+
+			@Override
+			public int getBatchSize() {
+				return items.size(); // or any other value you want
+			}
 		});
 	}
 
 
-	private String[] getFields() {
-		return new String[]{"id", "pseudonym", "gender", "birthdayFormatted", "zipCode", "city", "registrationDateFormatted"};
-	}
 }
